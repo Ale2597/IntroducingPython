03ad601ad41093e5c81bab3e421b6ac7f8792251
Alejandro Zeno <49791251+Ale2597@users.noreply.github.com>
2024-03-27 15:38:57 -0400

    Chapter 3 exercises.

---
+++ Chapter3/secondsPerHour.py
@@ -0,0 +1,8 @@
+seconds_per_hour = 60 * 60
+print(f"Seconds per hour: {seconds_per_hour}")
+
+seconds_per_day = seconds_per_hour * 24
+print(f"Seconds per day: {seconds_per_day}") 
+
+print(seconds_per_day/seconds_per_hour)
+print(seconds_per_day//seconds_per_hour)
