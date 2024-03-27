a25cd601edfd0e7ab637c34cf040612bc458cf5b
Alejandro Zeno <49791251+Ale2597@users.noreply.github.com>
2024-03-27 16:28:27 -0400

    Chapter 4 Exercises.

---
+++ Chapter4/if_elif_else.py
@@ -0,0 +1,9 @@
+secretNumber = 5
+guessNumber = 3
+
+if guessNumber < secretNumber:
+    print("Too low...")
+elif guessNumber > secretNumber:
+    print("Too high...")
+else:
+    print("Just right!")
---
+++ Chapter4/if_elif_else2.py
@@ -0,0 +1,13 @@
+green = False
+small = False
+
+if small:
+    if green:
+        print("It's a Pea!")
+    else:
+        print("It's a Cherry!")
+else:
+    if green:
+        print("It's a Watermelon!")
+    else:
+        print("It's a Pumpkin!")
