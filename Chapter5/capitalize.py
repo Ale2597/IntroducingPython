4829c9933be16c99f24d9992cb0b803b7c2ba806
Alejandro Zeno <49791251+Ale2597@users.noreply.github.com>
2024-04-10 22:58:48 -0400

    Revert "Chapter 5 Exercises."
    This reverts commit a62f1bc01e630284f6091ebe2ae674a782b726f6.

--- Chapter5/capitalize.py
+++
@@ -1,5 +0,0 @@
-song = """When an eel grabs your arm,
-And it cause great harm,
-That's - a moray!"""
-
-print(song.split(" ")[-1].capitalize())
--- Chapter5/old_style_formatting.py
+++
@@ -1,9 +0,0 @@
-thing1 = "roast beef"
-thing2 = "ham"
-thing3 = "head"
-thing4 = "clam"
-
-print("My kitty cat likes %s" % thing1)
-print("My kitty cat likes %s" % thing2)
-print("My kitty cat fell on this %s" % thing3)
-print("And now thinks he's on a %s" % thing4 + ".")
